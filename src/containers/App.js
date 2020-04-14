4d91083b3cfbf854747e50d60a86b9b649d43e26
dmitryglitch <brgdtv121>
2020-04-15 04:21:23 +0500

    Restructuring container App, connecting to store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
 // import logo from "../logo.svg";
 import "./App.css";
 import Chat from "../components/Chat/Chat.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="chat">
-        <div className="chat__container">
-          <div className="chat__container__item user">
-            <Chat type="user" />
-          </div>
-          <div className="chat__container__item supporter">
-            <Chat type="supporter" />
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="chat">
+          <div className="chat__container">
+            <div className="chat__container__item user">
+              <Chat type="user" />
+            </div>
+            <div className="chat__container__item supporter">
+              <Chat type="supporter" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
+export default connect(
+  state => ({}),
+  dispatch => ({})
+)(App);
