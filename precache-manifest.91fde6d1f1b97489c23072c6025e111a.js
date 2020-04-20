bd0cde22179f8de341a1236f86bec6019c7c41af
dmitryglitch <brgdtv121>
2020-04-20 21:48:15 +0500

    Updates

---
+++ precache-manifest.91fde6d1f1b97489c23072c6025e111a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697e49396ad5ef37e092871791d0bc12",
+    "url": "/ChatInLocalStorage/index.html"
+  },
+  {
+    "revision": "a88cdb7d0e70f3959f91",
+    "url": "/ChatInLocalStorage/static/css/main.eb79979c.chunk.css"
+  },
+  {
+    "revision": "bf8c48e7d1cb66aa26b1",
+    "url": "/ChatInLocalStorage/static/js/2.932c613d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChatInLocalStorage/static/js/2.932c613d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a88cdb7d0e70f3959f91",
+    "url": "/ChatInLocalStorage/static/js/main.e77596e6.chunk.js"
+  },
+  {
+    "revision": "6872528190bc496fdc53",
+    "url": "/ChatInLocalStorage/static/js/runtime-main.213247d7.js"
+  }
+]);
